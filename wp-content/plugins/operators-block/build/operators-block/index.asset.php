f320281bee45584ce353d65dd8764d02278d1fdf
James Borg <jamesborg2012@gmail.com>
2025-01-14 22:56:51 +0100

    Restructuring of block

--- wp-content/plugins/operators-block/build/operators-block/index.asset.php
+++ wp-content/plugins/operators-block/build/operators-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '712fa26316905382d848');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '4a395a68b89eaa7f3f32');
